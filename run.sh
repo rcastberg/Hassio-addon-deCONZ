95b225b13f89019a7889703e3823511d577d0494
Rene Castberg <rene@castberg.org>
2018-01-30 10:23:01 +0100

    Updated libraries and moved deconz to script

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 echo "deCONZ started"
+
+wget -N http://www.dresden-elektronik.de/rpi/deconz/deconz-latest.deb
+dpkg -i deconz-latest.deb
+
+sed -i -- 's/$APPNAME $OPTIONS $EXTRA_OPTIONS/xvfb-run $APPNAME $OPTIONS $EXTRA_OPTIONS/g' /usr/bin/deCONZ-autostart.sh
+
