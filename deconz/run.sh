cb3a09611304431c7b27e9bfab875575d5d269bb
Rene Castberg <rene@castberg.org>
2018-03-09 12:48:11 +0100

    Updated image to try and keep the db

--- deconz/run.sh
+++ deconz/run.sh
@@ -5,4 +5,6 @@ dpkg -i deconz-latest.deb
 
 chown root:root /usr/bin/deCONZ*
 echo "deCONZ starting"
+mkdir -p /root/.local/share/dresden-elektronik/deCONZ/
+ln -s /config/deconz.db  /root/.local/share/dresden-elektronik/deCONZ/zll.db
 /usr/bin/deCONZ -platform minimal --auto-connect=1 --dbg-info=1  --http-port=80
